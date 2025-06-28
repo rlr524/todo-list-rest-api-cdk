8d8a66224f0598415240a955e920d95f321e5bba
Rob Ranf <rob@emiyaconsulting.com>
2025-06-28 16:04:08 -0700

    Initial commit

---
+++ test/todo-list-rest-api-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TodoListRestApiCdk from '../lib/todo-list-rest-api-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/todo-list-rest-api-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TodoListRestApiCdk.TodoListRestApiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
